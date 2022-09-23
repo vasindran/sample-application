d5638ad2efa25404b114b59164b7ec78e8d15de4
vasindran <61245148+vasindran@users.noreply.github.com>
2022-09-23 11:53:24 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -16,6 +16,7 @@ class TestMyApp(unittest.TestCase):
         rv = self.app.get('/')
         assert rv.status == '200 OK'
         assert b'Congratulations' in rv.data
+        print("success")
         #assert False
 
     def test_404(self):
